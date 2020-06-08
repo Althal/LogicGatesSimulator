4a54fe23a79b5fa3c158651a64df2ef85ff862d1
Miłosz <45383427+Althal@users.noreply.github.com>
2020-06-08 09:43:20 +0200

    Add files via upload

--- src/logicgatessimulator/lib/SatSolver.java
+++ src/logicgatessimulator/lib/SatSolver.java
@@ -3,7 +3,6 @@ package logicgatessimulator.lib;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import javax.swing.JOptionPane;
 import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.reader.DimacsReader;
@@ -51,6 +50,7 @@ public class SatSolver {
             System.out.println(Arrays.toString(i));
             try{
                 if(i.length != 0) solver.addClause(new VecInt(i));
+                else return new int[] {0}; 
             }
             catch(Exception e){
                 return new int[] {0};
